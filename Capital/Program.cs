828c39ddf0b89474455056ff35eeaf14705a28da
gbenga.adaramodu <gbenga.adaramodu@cyberspace,net.ng>
2024-05-15 06:02:22 +0100

    Cosmos and appsetting  setup

--- Capital/Program.cs
+++ Capital/Program.cs
@@ -1,11 +1,41 @@
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+
 // Add services to the container.
+// Add CosmosClient and configure logging
+builder.Services.AddSingleton((provider) =>
+{
+    var endpointUri = configuration["ConnectionString:EndpointUri"];
+    var primaryKey = configuration["ConnectionString:PrimaryKey"];
+    var databaseName = configuration["ConnectionString:DatabaseName"];
+
+    var cosmosClientOptions = new CosmosClientOptions
+    {
+        ApplicationName = databaseName,
+        ConnectionMode = ConnectionMode.Gateway,
+    };
+
+    var loggerFactory = LoggerFactory.Create(builder =>
+    {
+        builder.AddConsole();
+    });
+
+    var cosmosClient = new CosmosClient(endpointUri, primaryKey, cosmosClientOptions);
+
+
+    return cosmosClient;
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpContextAccessor();
+
 
 var app = builder.Build();
 
